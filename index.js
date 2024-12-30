bf552603b179944133538a4da933374b565494d0
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2024-12-30 15:56:20 +0600

    nodejs project run succesfful

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-const express = require('express');
-const mongoose = require('mongoose')
-const app = express();
+const dotenv = require('dotenv')
+const colors = require('colors')
+const app  = require('./app')
 
+dotenv.config({path: "./config/.env"});
 
-mongoose.connect("mongodb+srv://MERN123:admin123@cluster0.elnly.mongodb.net/learn_node");
-
-const userSchema = new mongoose.Schema({
-      name: String,
-      age: Number,
-})
-const userModel = mongoose.model('emp',userSchema);
-app.listen('3001',()=>{
-    console.log('Server is running!..');
+app.listen(process.env.PORT, async() =>{
+    console.log(`Server is  running on http://localhost:${process.env.PORT}`.bgMagenta);
 })
