1d2409255cbb0c384f09728c2198f8ecdf9635d1
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2024-12-31 15:03:40 +0600

    nodejs layout

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const expressLayouts = require("express-ejs-layouts");
 const app = express();
 const blogRoutes = require("./routes/blog.routes");
 const userRoutes = require("./routes/user.routes");
@@ -9,10 +10,18 @@ const shopRoutes = require("./routes/shop.routes");
 
 app.use(express.json());
 app.use(bodyParser.json());
+app.use(expressLayouts);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.set("views", "views");
+
+app.get('/', (req, res) => {
+    res.render('home', { title: 'Home Page' });
+});
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'About Page', layout: 'app' })
+});
 
 
 app.use("/api", blogRoutes);
