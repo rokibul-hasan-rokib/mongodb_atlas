4d71e1b15958c7b67f8b2cd00ae16e682101ca61
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2024-12-31 10:41:59 +0600

    nodejs validation

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
 const app = express();
 const blogRoutes = require("./routes/blog.routes");
 const userRoutes = require("./routes/user.routes");
 
 
 app.use(express.json());
+app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/user.controller");
+const { body } = require("express-validator");
 
 router.get("/user", userController.getAllUsers);
-router.post("/user", userController.userRegister);
+router.post("/user",
+    [
+        body('email').isEmail().withMessage('Invalid Email'),
+        body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
+    ], 
+    userController.userRegister);
 router.post("/user/login", userController.userLogin);
 router.get("/user/:id", userController.getUserById);   
 router.put("/user/:id", userController.updateUser);
